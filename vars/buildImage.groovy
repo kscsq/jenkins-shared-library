695a2a40fc3bbccb6d0c6e0ac09c7ffcdc7a2162
FYuDmitriev <fedor_dmitriev@rgs.ru>
2022-08-06 17:35:40 +0300

    Add class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-package com.example
+import com.example.Docker
 
 def call(String imageName) {
     return new Docker(this).buildDockerImage(imageName)
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,5 +1,4 @@
 #!/usr/bin/env groovy
-package com.example
 
 def call() {
     echo "building the application for branch $BRANCH_NAME"
